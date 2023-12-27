8d61bf0bb46f1720faf9550cf4db178cb6a9741e
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-12-27 12:18:36 +0000

    test: improve tests for reactive aggregate

--- dsl/src/commonTest/kotlin/it/unibo/collektive/reactive/Test.kt
+++ dsl/src/commonTest/kotlin/it/unibo/collektive/reactive/Test.kt
@@ -18,60 +18,71 @@ import kotlinx.coroutines.launch
 
 class Test : StringSpec({
 
-    // device ids
     val id1 = IntId(1)
     val id2 = IntId(2)
     val id3 = IntId(3)
 
     // initial values
-    val initialValue = 100
+    val initV1 = 1
+    val initV2 = 2
+    val initV3 = 3
 
     val increaseOrDouble: (Field<Int>) -> Field<Int> = { f ->
         f.mapWithId { _, v -> if (v % 2 == 0) v + 1 else v * 2 }
     }
 
-    suspend fun <R> runSimulation(ids: Iterable<IntId>, program: AggregateContext.() -> R) = coroutineScope {
+    suspend fun <R> runSimulation(simulation: Map<IntId, AggregateContext.() -> R>) = coroutineScope {
         println("#################################")
-        val inboundMessages = ids.associateWith { MutableStateFlow(emptyList<InboundMessage>()) }
-        val aggregateResults = ids.associateWith {
-            aggregate(it, inboundMessages[it]!!.asStateFlow(), program)
+        val inboundMessages = simulation.map { (k, _) -> k to MutableStateFlow(emptyList<InboundMessage>()) }.toMap()
+        val aggregateResults = simulation.map { (k, v) ->
+            k to aggregate(k, inboundMessages[k]!!.asStateFlow(), v)
         }
-        val jobs = aggregateResults.map { (selfId, aggregateResultFlow) ->
+        val jobs = aggregateResults.associate { (selfId, aggregateResultFlow) ->
             selfId to
                 launch(Dispatchers.Default) {
                     aggregateResultFlow.collect { aggregateResult ->
-                        println("$aggregateResult")
+                        println("${aggregateResult.localId} = ${aggregateResult.result}")
                         inboundMessages.filter { (k, _) -> k != selfId }.values.forEach { neighborsInboundMessages ->
-                            neighborsInboundMessages.update { messages ->
-                                messages.filter { it.senderId != selfId } +
-                                    InboundMessage(
-                                        aggregateResult.toSend.senderId,
-                                        aggregateResult.toSend.messages.mapValues { (_, single) ->
-                                            single.overrides.getOrElse(aggregateResult.toSend.senderId) { single.default }
-                                        },
-                                    )
+                            neighborsInboundMessages.update { inboundMessages ->
+                                val message = InboundMessage(
+                                    aggregateResult.toSend.senderId,
+                                    aggregateResult.toSend.messages.mapValues { (_, single) ->
+                                        single.overrides.getOrElse(aggregateResult.toSend.senderId) { single.default }
+                                    },
+                                )
+                                inboundMessages.filter { it.senderId != selfId } + message
                             }
                         }
                     }
                 }
-        }.toMap()
+        }
         delay(50)
         jobs.forEach { it.value.cancelAndJoin() }
     }
 
+    fun exchangeProgram(value: Int): AggregateContext.() -> Field<Int> {
+        return { exchange(value, increaseOrDouble) }
+    }
+
     "First time exchange should return the initial value" {
         runBlocking {
-            runSimulation(listOf(id1)) {
-                exchange(initialValue, increaseOrDouble)
-            }
+            runSimulation(
+                mapOf(
+                    id1 to exchangeProgram(initV1),
+                )
+            )
         }
     }
 
     "Exchange should work between three aligned devices" {
         runBlocking {
-            runSimulation(listOf(id1, id2, id3)) {
-                exchange(initialValue, increaseOrDouble)
-            }
+            runSimulation(
+                mapOf(
+                    id1 to exchangeProgram(initV1),
+                    id2 to exchangeProgram(initV2),
+                    id3 to exchangeProgram(initV3),
+                )
+            )
         }
     }
 })
