b435377ba2f055c1ea12790e89945e05f7898c61
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-12-28 13:59:35 +0000

    test: add assertions in it.unibo.collektive.reactive.ExchangeTest

--- dsl/src/commonTest/kotlin/it/unibo/collektive/reactive/ExchangeTest.kt
+++ dsl/src/commonTest/kotlin/it/unibo/collektive/reactive/ExchangeTest.kt
@@ -19,14 +19,23 @@ class ExchangeTest : StringSpec({
 
     // initial values
     val initV1 = 1
+    val initV2 = 2
     val initV3 = 3
+    val initV4 = 4
     val initV5 = 5
+    val initV6 = 6
 
     // paths
     val path1 = Path(listOf("invoke.1", "exchange.1"))
+    val path2 = Path(listOf("invoke.1", "exchange.2"))
 
     // expected
     val expected2 = 2
+    val expected3 = 3
+    val expected5 = 5
+    val expected6 = 6
+    val expected7 = 7
+    val expected10 = 10
 
     val increaseOrDouble: (Field<Int>) -> Field<Int> = { f ->
         f.mapWithId { _, v -> if (v % 2 == 0) v + 1 else v * 2 }
@@ -56,15 +65,65 @@ class ExchangeTest : StringSpec({
                 mapOf(
                     // Device 1
                     id1 to {
-                        exchange(initV1, increaseOrDouble)
+                        val res1 = exchange(initV1, increaseOrDouble)
+                        val res2 = exchange(initV2, increaseOrDouble)
+                        res1.localValue shouldBe expected2
+                        res2.localValue shouldBe expected3
+                        messagesToSend() shouldBe OutboundMessage(
+                            id1,
+                            mapOf(
+                                path1 to SingleOutboundMessage(expected2, emptyMap()),
+                                path2 to SingleOutboundMessage(expected3, emptyMap()),
+                            ),
+                        )
                     },
                     // Device 2
                     id2 to {
-                        exchange(initV3, increaseOrDouble)
+                        val res1 = exchange(initV3, increaseOrDouble)
+                        val res2 = exchange(initV4, increaseOrDouble)
+
+                        res1.localValue shouldBe expected6
+                        res2.localValue shouldBe expected5
+                        messagesToSend() shouldBe OutboundMessage(
+                            id2,
+                            mapOf(
+                                path1 to SingleOutboundMessage(
+                                    expected6,
+                                    mapOf(id1 to expected3),
+                                ),
+                                path2 to SingleOutboundMessage(
+                                    expected5,
+                                    mapOf(id1 to expected6),
+                                ),
+                            ),
+                        )
                     },
                     // Device 3
                     id3 to {
-                        exchange(initV5, increaseOrDouble)
+                        val res1 = exchange(initV5, increaseOrDouble)
+                        val res2 = exchange(initV6, increaseOrDouble)
+
+                        res1.localValue shouldBe expected10
+                        res2.localValue shouldBe expected7
+                        messagesToSend() shouldBe OutboundMessage(
+                            id3,
+                            mapOf(
+                                path1 to SingleOutboundMessage(
+                                    expected10,
+                                    mapOf(
+                                        id1 to expected3,
+                                        id2 to expected7,
+                                    ),
+                                ),
+                                path2 to SingleOutboundMessage(
+                                    expected7,
+                                    mapOf(
+                                        id1 to expected6,
+                                        id2 to expected10,
+                                    ),
+                                ),
+                            ),
+                        )
                     },
                 )
             )
