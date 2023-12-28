ad1999ea13d2fd2eb670f97d09b853f2602818a2
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-12-28 13:58:29 +0000

    test: switch from delay to take in MockSimulator

--- dsl/src/commonTest/kotlin/it/unibo/collektive/reactive/MockSimulator.kt
+++ dsl/src/commonTest/kotlin/it/unibo/collektive/reactive/MockSimulator.kt
@@ -5,28 +5,19 @@ import it.unibo.collektive.IntId
 import it.unibo.collektive.aggregate.AggregateContext
 import it.unibo.collektive.network.ReactiveNetworkImplTest
 import it.unibo.collektive.network.ReactiveNetworkManager
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.cancelAndJoin
 import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.take
 
-class MockSimulator<R>(val simulation: Map<IntId, AggregateContext.() -> R>) {
+class MockSimulator<R>(private val simulation: Map<IntId, AggregateContext.() -> R>) {
     suspend fun runSimulation() = coroutineScope {
         val networkManager = ReactiveNetworkManager()
         val networks = simulation.keys.associateWith { ReactiveNetworkImplTest(networkManager, it) }
         val aggregateResults = simulation.map { (k, v) ->
             k to Collektive.aggregate(k, networks[k]!!, v)
         }.toMap()
-        val jobs = aggregateResults.map { (selfId, aggregateResultFlow) ->
+        aggregateResults.map { (selfId, aggregateResultFlow) ->
             selfId to
-                launch(Dispatchers.Default) {
-                    aggregateResultFlow.collect {
-                        println("$selfId -> ${it.result}")
-                    }
-                }
+                aggregateResultFlow.take(1).collect {}
         }.toMap()
-        delay(50)
-        jobs.values.forEach { it.cancelAndJoin() }
     }
 }
