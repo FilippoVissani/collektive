8056f533e6002e44348a9a806b7e5dc796c6808d
Filippo Vissani <filippo.vissani@studio.unibo.it>
2024-01-08 11:42:49 +0000

    test: add assertions in reactive tests

--- dsl/src/commonTest/kotlin/it/unibo/collektive/reactive/ExchangeTest.kt
+++ dsl/src/commonTest/kotlin/it/unibo/collektive/reactive/ExchangeTest.kt
@@ -2,10 +2,12 @@ package it.unibo.collektive.reactive
 
 import io.kotest.common.runBlocking
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
 import it.unibo.collektive.Collektive.aggregate
 import it.unibo.collektive.IntId
 import it.unibo.collektive.field.Field
 import it.unibo.collektive.reactive.flow.extensions.mapStates
+import it.unibo.collektive.stack.Path
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.cancelAndJoin
 import kotlinx.coroutines.coroutineScope
@@ -26,6 +28,21 @@ class ExchangeTest : StringSpec({
     val initV1 = 1
     val initV2 = 2
     val initV3 = 3
+    val trueCondition = true
+    val falseCondition = false
+
+    // paths
+    val path1 = Path(listOf("invoke.1", "exchange.1"))
+    val truePath = Path(listOf("invoke.1", "true", "exchange.1"))
+    val falsePath = Path(listOf("invoke.1", "false", "exchange.1"))
+
+    // expected
+    val expected2 = 2
+    val expected3 = 3
+    val expected6 = 6
+    val expected7 = 7
+    val expectedTrue = true
+    val expectedFalse = false
 
     val increaseOrDouble: (StateFlow<Field<Int>>) -> StateFlow<Field<Int>> = { flow ->
         mapStates(flow) { field ->
@@ -67,7 +84,7 @@ class ExchangeTest : StringSpec({
         }
     }
 
-    suspend fun <R> runSimulation(simulation: Map<ReactiveAggregateResult<R>, MutableStateFlow<List<InboundMessage>>>) = coroutineScope {
+    suspend fun <R> runSimulation(simulation: Map<ReactiveAggregateResult<R>, MutableStateFlow<List<ReactiveInboundMessage>>>) = coroutineScope {
         simulation.keys.forEach {
             launch(Dispatchers.Default) {
                 printResults(it)
@@ -84,7 +101,7 @@ class ExchangeTest : StringSpec({
                                 channel.update { inboundMessages ->
                                     // if this device already sent a message, that message is updated. New message is generated otherwise.
                                     if (inboundMessages.none { it.senderId == aggregateResult.localId }) {
-                                        val newMessage = InboundMessage(
+                                        val newMessage = ReactiveInboundMessage(
                                             aggregateResult.localId,
                                             mapOf(path to message.overrides.getOrElse(aggregateResult.localId) { message.default })
                                         )
@@ -92,7 +109,7 @@ class ExchangeTest : StringSpec({
                                     } else {
                                         val oldMessage =
                                             inboundMessages.first { it.senderId == aggregateResult.localId }
-                                        val newMessage = InboundMessage(
+                                        val newMessage = ReactiveInboundMessage(
                                             oldMessage.senderId,
                                             oldMessage.messages + (path to message.overrides.getOrElse(aggregateResult.localId) { message.default }),
                                         )
@@ -111,20 +128,30 @@ class ExchangeTest : StringSpec({
             val aggregateResult0 = aggregate(id0) {
                 exchange(initV1, increaseOrDouble)
             }
-            println("#############################")
             val job = launch(Dispatchers.Default) {
                 runSimulation(mapOf(aggregateResult0 to MutableStateFlow(emptyList())))
             }
             delay(100)
             job.cancelAndJoin()
+            aggregateResult0
+                .result
+                .value
+                .localValue shouldBe expected2
+            aggregateResult0
+                .toSend
+                .senderId shouldBe id0
+            aggregateResult0
+                .toSend
+                .messages
+                .map { (k, v) -> k to v.value } shouldBe mapOf(path1 to ReactiveSingleOutboundMessage(expected2, emptyMap()))
         }
     }
 
     "Exchange should work between three aligned devices" {
         runBlocking {
-            val channel0: MutableStateFlow<List<InboundMessage>> = MutableStateFlow(emptyList())
-            val channel1: MutableStateFlow<List<InboundMessage>> = MutableStateFlow(emptyList())
-            val channel2: MutableStateFlow<List<InboundMessage>> = MutableStateFlow(emptyList())
+            val channel0: MutableStateFlow<List<ReactiveInboundMessage>> = MutableStateFlow(emptyList())
+            val channel1: MutableStateFlow<List<ReactiveInboundMessage>> = MutableStateFlow(emptyList())
+            val channel2: MutableStateFlow<List<ReactiveInboundMessage>> = MutableStateFlow(emptyList())
 
             val aggregateResult0 = aggregate(id0, channel0) {
                 exchange(initV1, increaseOrDouble)
@@ -137,7 +164,6 @@ class ExchangeTest : StringSpec({
             val aggregateResult2 = aggregate(id2, channel2) {
                 exchange(initV3, increaseOrDouble)
             }
-            println("#############################")
             val job = launch(Dispatchers.Default) {
                 runSimulation(
                     mapOf(
@@ -149,22 +175,56 @@ class ExchangeTest : StringSpec({
             }
             delay(100)
             job.cancelAndJoin()
+            aggregateResult0
+                .result
+                .value
+                .localValue shouldBe expected2
+            aggregateResult0
+                .toSend
+                .senderId shouldBe id0
+            aggregateResult0
+                .toSend
+                .messages
+                .map { (k, v) -> k to v.value } shouldBe mapOf(path1 to ReactiveSingleOutboundMessage(expected2, mapOf((id2 to expected7), (id1 to expected6))))
+
+            aggregateResult1
+                .result
+                .value
+                .localValue shouldBe expected3
+            aggregateResult1
+                .toSend
+                .senderId shouldBe id1
+            aggregateResult1
+                .toSend
+                .messages
+                .map { (k, v) -> k to v.value } shouldBe mapOf(path1 to ReactiveSingleOutboundMessage(expected3, mapOf((id0 to expected3), (id2 to expected7))))
+
+            aggregateResult2
+                .result
+                .value
+                .localValue shouldBe expected6
+            aggregateResult2
+                .toSend
+                .senderId shouldBe id2
+            aggregateResult2
+                .toSend
+                .messages
+                .map { (k, v) -> k to v.value } shouldBe mapOf(path1 to ReactiveSingleOutboundMessage(expected6, mapOf((id1 to expected6), (id0 to expected3))))
         }
     }
 
     "Devices should be aligned" {
         runBlocking {
-            val channel0: MutableStateFlow<List<InboundMessage>> = MutableStateFlow(emptyList())
-            val channel1: MutableStateFlow<List<InboundMessage>> = MutableStateFlow(emptyList())
+            val channel0: MutableStateFlow<List<ReactiveInboundMessage>> = MutableStateFlow(emptyList())
+            val channel1: MutableStateFlow<List<ReactiveInboundMessage>> = MutableStateFlow(emptyList())
 
             val aggregateResult0 = aggregate(id0, channel0) {
-                if(true) exchange(true, alwaysTrue) else exchange(false, alwaysFalse)
+                if (trueCondition) exchange(true, alwaysTrue) else exchange(false, alwaysFalse)
             }
 
             val aggregateResult1 = aggregate(id1, channel1) {
-                if(true) exchange(true, alwaysTrue) else exchange(false, alwaysFalse)
+                if (trueCondition) exchange(true, alwaysTrue) else exchange(false, alwaysFalse)
             }
-            println("#############################")
             val job = launch(Dispatchers.Default) {
                 runSimulation(
                     mapOf(
@@ -175,22 +235,44 @@ class ExchangeTest : StringSpec({
             }
             delay(100)
             job.cancelAndJoin()
+            aggregateResult0
+                .result
+                .value
+                .localValue shouldBe expectedTrue
+            aggregateResult0
+                .toSend
+                .senderId shouldBe id0
+            aggregateResult0
+                .toSend
+                .messages
+                .map { (k, v) -> k to v.value } shouldBe mapOf(truePath to ReactiveSingleOutboundMessage(expectedTrue, mapOf((id1 to expectedTrue))))
+
+            aggregateResult1
+                .result
+                .value
+                .localValue shouldBe expectedTrue
+            aggregateResult1
+                .toSend
+                .senderId shouldBe id1
+            aggregateResult1
+                .toSend
+                .messages
+                .map { (k, v) -> k to v.value } shouldBe mapOf(truePath to ReactiveSingleOutboundMessage(expectedTrue, mapOf((id0 to expectedTrue))))
         }
     }
 
     "Devices should not be aligned" {
         runBlocking {
-            val channel0: MutableStateFlow<List<InboundMessage>> = MutableStateFlow(emptyList())
-            val channel1: MutableStateFlow<List<InboundMessage>> = MutableStateFlow(emptyList())
+            val channel0: MutableStateFlow<List<ReactiveInboundMessage>> = MutableStateFlow(emptyList())
+            val channel1: MutableStateFlow<List<ReactiveInboundMessage>> = MutableStateFlow(emptyList())
 
             val aggregateResult0 = aggregate(id0, channel0) {
-                if(true) exchange(true, alwaysTrue) else exchange(false, alwaysFalse)
+                if (trueCondition) exchange(true, alwaysTrue) else exchange(false, alwaysFalse)
             }
 
             val aggregateResult1 = aggregate(id1, channel1) {
-                if(false) exchange(true, alwaysTrue) else exchange(false, alwaysFalse)
+                if (falseCondition) exchange(true, alwaysTrue) else exchange(false, alwaysFalse)
             }
-            println("#############################")
             val job = launch(Dispatchers.Default) {
                 runSimulation(
                     mapOf(
@@ -201,6 +283,29 @@ class ExchangeTest : StringSpec({
             }
             delay(100)
             job.cancelAndJoin()
+            aggregateResult0
+                .result
+                .value
+                .localValue shouldBe expectedTrue
+            aggregateResult0
+                .toSend
+                .senderId shouldBe id0
+            aggregateResult0
+                .toSend
+                .messages
+                .map { (k, v) -> k to v.value } shouldBe mapOf(truePath to ReactiveSingleOutboundMessage(expectedTrue, emptyMap()))
+
+            aggregateResult1
+                .result
+                .value
+                .localValue shouldBe expectedFalse
+            aggregateResult1
+                .toSend
+                .senderId shouldBe id1
+            aggregateResult1
+                .toSend
+                .messages
+                .map { (k, v) -> k to v.value } shouldBe mapOf(falsePath to ReactiveSingleOutboundMessage(expectedFalse, emptyMap()))
         }
     }
 })
