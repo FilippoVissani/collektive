090796c2ef32e96fa61c9ef8dd1bac85e517cb18
Filippo Vissani <filippo.vissani@studio.unibo.it>
2024-01-13 10:55:41 +0000

    feat: add fun <T1, T2, T3, R> combineStates

--- dsl/src/commonMain/kotlin/it/unibo/collektive/reactive/flow/extensions/StateFlowExtensions.kt
+++ dsl/src/commonMain/kotlin/it/unibo/collektive/reactive/flow/extensions/StateFlowExtensions.kt
@@ -51,3 +51,26 @@ fun <T1, T2, R> combineStates(
     getValue = { transform(stateFlow1.value, stateFlow2.value) },
     flow = combine(stateFlow1, stateFlow2) { value1, value2 -> transform(value1, value2) }
 )
+
+/**
+ * TODO.
+ *
+ * @param T1
+ * @param T2
+ * @param T3
+ * @param R
+ * @param stateFlow1
+ * @param stateFlow2
+ * @param stateFlow3
+ * @param transform
+ * @return
+ */
+fun <T1, T2, T3, R> combineStates(
+    stateFlow1: StateFlow<T1>,
+    stateFlow2: StateFlow<T2>,
+    stateFlow3: StateFlow<T3>,
+    transform: (T1, T2, T3) -> R
+): StateFlow<R> = combineStates(
+    getValue = { transform(stateFlow1.value, stateFlow2.value, stateFlow3.value) },
+    flow = combine(stateFlow1, stateFlow2, stateFlow3) { value1, value2, value3 -> transform(value1, value2, value3) }
+)
