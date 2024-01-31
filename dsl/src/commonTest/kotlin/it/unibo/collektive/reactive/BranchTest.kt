4bf48932ba66d6277cb5b120bed88b4fbd888453
Filippo Vissani <filippo.vissani@studio.unibo.it>
2024-01-31 09:32:59 +0000

    test: improve Branch tests

--- dsl/src/commonTest/kotlin/it/unibo/collektive/reactive/BranchTest.kt
+++ dsl/src/commonTest/kotlin/it/unibo/collektive/reactive/BranchTest.kt
@@ -4,14 +4,15 @@ import io.kotest.common.runBlocking
 import io.kotest.core.spec.style.StringSpec
 import it.unibo.collektive.Collektive
 import it.unibo.collektive.IntId
-import it.unibo.collektive.aggregate.branch
 import it.unibo.collektive.field.Field
 import it.unibo.collektive.reactive.flow.extensions.mapStates
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.cancelAndJoin
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
 class BranchTest : StringSpec({
@@ -32,36 +33,84 @@ class BranchTest : StringSpec({
         }
     }
 
+    suspend fun aggregateProgram(
+        initialCondition0: Boolean,
+        initialCondition1: Boolean,
+        finalCondition0: Boolean = initialCondition0,
+        finalCondition1: Boolean = initialCondition1,
+    ) = coroutineScope {
+        val channel0: MutableStateFlow<List<ReactiveInboundMessage>> = MutableStateFlow(emptyList())
+        val channel1: MutableStateFlow<List<ReactiveInboundMessage>> = MutableStateFlow(emptyList())
+        val reactiveBoolean0 = MutableStateFlow(initialCondition0)
+        val reactiveBoolean1 = MutableStateFlow(initialCondition1)
+
+        val aggregateResult0 = Collektive.aggregate(id0, channel0) {
+            branch(
+                { reactiveBoolean0 },
+                { exchange("initial", trueFunction) },
+                { exchange("initial", falseFunction) },
+            )
+        }
+
+        val aggregateResult1 = Collektive.aggregate(id1, channel1) {
+            branch(
+                { reactiveBoolean1 },
+                { exchange("initial", trueFunction) },
+                { exchange("initial", falseFunction) },
+            )
+        }
+        val job = launch(Dispatchers.Default) {
+            runSimulation(
+                mapOf(
+                    aggregateResult0 to channel0,
+                    aggregateResult1 to channel1,
+                )
+            )
+        }
+        delay(100)
+        reactiveBoolean0.update { finalCondition0 }
+        reactiveBoolean1.update { finalCondition1 }
+        delay(100)
+        job.cancelAndJoin()
+    }
+
     "Devices with same condition should be aligned" {
         runBlocking {
-            val channel0: MutableStateFlow<List<ReactiveInboundMessage>> = MutableStateFlow(emptyList())
-            val channel1: MutableStateFlow<List<ReactiveInboundMessage>> = MutableStateFlow(emptyList())
-            val reactiveBoolean0 = MutableStateFlow(true)
-            val reactiveBoolean1 = MutableStateFlow(true)
+            aggregateProgram(
+                initialCondition0 = true,
+                initialCondition1 = true,
+            )
+        }
+    }
 
-            val aggregateResult0 = Collektive.aggregate(id0, channel0) {
-                reactiveBoolean0.branch(
-                    { exchange("initial", trueFunction) },
-                    { exchange("initial", falseFunction) },
-                )
-            }
+    "Devices with different conditions should not be aligned" {
+        runBlocking {
+            aggregateProgram(
+                initialCondition0 = true,
+                initialCondition1 = false,
+            )
+        }
+    }
 
-            val aggregateResult1 = Collektive.aggregate(id1, channel1) {
-                reactiveBoolean1.branch(
-                    { exchange("initial", trueFunction) },
-                    { exchange("initial", falseFunction) },
-                )
-            }
-            val job = launch(Dispatchers.Default) {
-                runSimulation(
-                    mapOf(
-                        aggregateResult0 to channel0,
-                        aggregateResult1 to channel1,
-                    )
-                )
-            }
-            delay(100)
-            job.cancelAndJoin()
+    "If the condition becomes the same the two devices should align" {
+        runBlocking {
+            aggregateProgram(
+                initialCondition0 = true,
+                initialCondition1 = false,
+                finalCondition0 = true,
+                finalCondition1 = true,
+            )
+        }
+    }
+
+    "If the condition becomes different the two devices should not align" {
+        runBlocking {
+            aggregateProgram(
+                initialCondition0 = true,
+                initialCondition1 = true,
+                finalCondition0 = true,
+                finalCondition1 = false,
+            )
         }
     }
 })
