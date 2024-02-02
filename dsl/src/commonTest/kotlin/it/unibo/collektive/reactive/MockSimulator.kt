02232722584909308e668a8495a4d182f12cdb91
Filippo Vissani <filippo.vissani@studio.unibo.it>
2024-02-02 10:56:11 +0000

    chore: fix imports

--- dsl/src/commonTest/kotlin/it/unibo/collektive/reactive/MockSimulator.kt
+++ dsl/src/commonTest/kotlin/it/unibo/collektive/reactive/MockSimulator.kt
@@ -4,7 +4,6 @@ import it.unibo.collektive.proactive.networking.InboundMessage
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
